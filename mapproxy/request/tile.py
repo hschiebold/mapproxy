b268e199bc204fec14441e3914b58d02a92d7e36
hschiebold <holger.schiebold@gmx.de>
2025-06-20 15:16:37 +0200

    check against /tms/1.0.0 path for cap req

--- mapproxy/request/tile.py
+++ mapproxy/request/tile.py
@@ -81,7 +81,7 @@ class TMSRequest(TileRequest):
     request_handler_name = 'map'
     req_prefix = '/tms'
     capabilities_re = re.compile(r'''
-        ^.*/1\.0\.0/?
+        ^/tms/1\.0\.0/?
         (/(?P<layer>[^/]+))?
         (/(?P<layer_spec>[^/]+))?
         $''', re.VERBOSE)
